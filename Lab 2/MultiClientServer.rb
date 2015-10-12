1054bfa00bec0f5c3d94aac7f7065215926731fc
jpf8992 <mcconnjp@tcd.ie>
2015-10-12 19:33:13 +0100

    added server talking back to client

--- Lab 2/MultiClientServer.rb
+++ Lab 2/MultiClientServer.rb
@@ -2,12 +2,22 @@ require 'socket'
 
 server = TCPServer.new (2000)
 puts ("Server Started:  ")
-loop do
+
 while (insession = server.accept)
 	
-  Thread.start(server.accept) do |client|
-    client.puts "Hello !"
-    client.puts "Time is #{Time.now}"
-    client.close
+  #Thread.start(server.accept) do |client|
+  Thread.start do
+  
+	  #inform administrator of new connection
+	  puts "Received input from Client"
+	  
+	  #Retrieve message and output to terminal
+	  clientinput = insession.gets
+	  puts clientinput
+  
+	  #Send acknowledgment to client
+	  puts "closing connection with Client"
+	  insession.puts "Server: Farewell\n"
+	  
   end
 end
