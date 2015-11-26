a00b43a85b14a8a11f3571686928565cf195e01c
jpf8992 <mcconnjp@tcd.ie>
2015-11-26 00:10:48 +0000

    Working on Joining Clients

--- Lab 4/MultiServerLab4.rb
+++ Lab 4/MultiServerLab4.rb
@@ -33,7 +33,7 @@ StudentID = "7978aa6f13ccec45a0d2f2ab6929b607369312d75d7681d833f269172e89313f"
 		new_pool.schedule do  	#Schedule a new task to a thread
 		
 			message = connect.gets
-			puts message
+			#puts message
 			
 			#connect.puts "In Thread\n"
 			if message.include?"KILL_SERVICE"		
@@ -44,9 +44,55 @@ StudentID = "7978aa6f13ccec45a0d2f2ab6929b607369312d75d7681d833f269172e89313f"
 						
 						connect.puts "#{message}"+"IP:"+"#{hostname}\n"+"Port:#{portnumber}\n"+"StudentID:#{StudentID}\n"
 												
-			else 
-						puts "Message Not Processed?? \n"
+			elsif	message.include?"JOIN_CHATROOM" 	#Join Request
+					
+					puts message
+					
+					
+					
+					#ENSURE THAT YOU ONLY EXTRACT THE RELEVANT DETAILS FROM MESSAGE!!!!
+					#Process Join Request	
+					ChatName = message		#First Line - JOIN_CHATROOM: [chatroom name]
+					message = connect.gets
+					
+					puts message
+					
+					ClientIP = message 		#Second Line - CLIENT_IP: [IP Address of client if UDP | 0 if TCP]
+					message = connect.gets
+					PortNum = message 		#Third Line - PORT: [port number of client if UDP | 0 if TCP]
+					message = connect.gets
+					ClientName = message 	#Fourth Line - CLIENT_NAME: [string Handle to identifier client user]
+					blank = connect.gets
+					
+					#Check the values assigned to variables
+					puts ChatName 
+					puts ClientIP
+					puts PortNum
+					puts ClientName
+					puts blank
+					
+					
+					#Send Message Confirmation to Client
+					connect.puts "JOINED_CHATROOM: ["+"#{ChatName}"+"]\n"
+					connect.puts "SERVER_IP: ["+ "#{hostname}" + "]\n"
+					connect.puts "PORT: ["+ "#{portnumber}" + "]\n"
+					connect.puts "ROOM_REF: ["+ "#{123}" + "]\n"
+					connect.puts "JOIN_ID: ["+ "#{321}" + "]\n"
+					
+					#JOINED_CHATROOM: [chatroom name]
+					#SERVER_IP: [IP address of chat room]
+					#PORT: [port number of chat room]
+					#ROOM_REF: [integer that uniquely identifies chat room on server]
+					#JOIN_ID: [integer that uniquely identifies client joining]
+										
+					
+			else
+			
+					puts "Message Not Processed?? \n"
+					#puts message
+					#connect.puts "JOINED_CHATROOM: \n"
 			end
+			
 			
 			connect.close
 			puts "Connection Closed \n Awaiting New Client..."
